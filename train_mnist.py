886cc3eb9c866050f272f7413573f7f27d803750
unknown <michalkozak2@o2.pl>
2022-06-10 21:48:30 +0200

    zmiana kodu sieci mtcnn

--- train_mnist.py
+++ train_mnist.py
@@ -53,9 +53,9 @@ class PyNet(nn.Module):
         x = self.fc2(x)
         output = F.log_softmax(x, dim=1)
         return output"""
-    def __init__(self):
+    def __init__(self, in_channels=1):
         super(PyNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv1 = nn.Conv2d(in_channels, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d(p=0.15)
         self.fc1 = nn.Linear(320, 20)
